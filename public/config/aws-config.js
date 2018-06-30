fc65f9a5c858c6a0ed32a9ef04a6f860b7b241af
Đạt Lê Thành <datlt.uit@gmail.com>
2018-06-30 14:08:16 +0700

    init node

---
+++ public/config/aws-config.js
@@ -0,0 +1,9 @@
+// Example of various AWS Mobile Hub constants used by this project
+
+// NOTE: AWS Mobile Hub will generate an aws-config file including these
+// constants configured with the values of your project. You should use that
+// file instead of this one. This is meant to show a sample of that file.
+
+const aws_bots_config = '[{"name":"BotFeedback","alias":"$LATEST","description":"Bot to get feedback.","bot-template":"bot-flowers","commands-help":["I would like to give feedback","I would like to do survey"],"region":"us-east-1"}]';
+const aws_cognito_identity_pool_id = 'us-east-1:dfcdec73-d992-4f3f-8195-db820649d753';
+const aws_cognito_region = 'us-east-1';
